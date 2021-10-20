7f9c9c08481a7893baaaf407850aebbbee9830db
Tymee Kong <tymeekong562@gmail.com>
2021-10-20 11:48:01 -0700

    Finished ship class

--- src/logic/ship.js
+++ src/logic/ship.js
@@ -1,12 +1,30 @@
-// const ship = () => {
-//     console.log('Hello World');
-//     return {
-//         'Hello World';
-//     }
-// };
+const ship = (hitPoints = 0) => {
+    let hp = [];
 
-function ship() {
-    return 'Hello World';
-}
+    const createShipArray = function () {
+        for (let i = 0; i < hitPoints; i++) {
+            hp.push(1);
+        }
+    };
+    createShipArray();
+    return {
+        isSunk: () => {
+            return hp.every(value => value === 0);
+        },
+        hit: hit => {
+            hp[hit] = 0;
+        },
+        get length() {
+            return hp.length;
+        },
+        get hp() {
+            return hp;
+        },
+    };
+};
+
+// function ship() {
+//     return 'Hello World';
+// }
 
 module.exports = ship;
--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,5 +1,45 @@
 const ship = require('../logic/ship');
+let battleship;
 
-it('Tests to see if it works', () => {
-    expect(ship()).toBe('Hello World');
+beforeEach(() => {
+    battleship = ship(4);
+    battleship.hp = [1, 1, 1, 1];
+});
+
+afterEach(() => {
+    battleship = ship(4);
+    battleship.hp = [1, 1, 1, 1];
+});
+
+it('Ship has full health', () => {
+    expect(battleship.hp).toStrictEqual([1, 1, 1, 1]);
+});
+
+it('Ship has a sunk boolean', () => {
+    expect(battleship.isSunk()).toBeDefined();
+});
+
+it('Ship has a length', () => {
+    expect(battleship.length).toBeTruthy();
+});
+
+it('Ship is hit at a certain location', () => {
+    battleship.hit(3);
+    battleship.hit(0);
+    expect(battleship.hp).toStrictEqual([0, 1, 1, 0]);
+});
+
+it('Ship is hit multiple times', () => {
+    battleship.hit(1);
+    battleship.hit(0);
+    battleship.hit(2);
+    expect(battleship.hp).toStrictEqual([0, 0, 0, 1]);
+});
+
+it('Ship is dead', () => {
+    battleship.hit(0);
+    battleship.hit(1);
+    battleship.hit(2);
+    battleship.hit(3);
+    expect(battleship.isSunk()).toBeTruthy();
 });
