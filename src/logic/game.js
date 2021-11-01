ad46d3b8dcef57d6f77859d93f07965ea5d21be1
Tymee Kong <tymeekong562@gmail.com>
2021-11-01 13:14:39 -0700

    Added styling

--- src/logic/game.js
+++ src/logic/game.js
@@ -5,4 +5,25 @@ const ship = require('./ship');
 const gameLoop = () => {
     const humanPlayer = player();
     const aiPlayer = player();
+    const humanGameBoard = gameBoard();
+    const aiGameBoard = gameBoard();
+    let currentPlayer = 1;
+
+    return {
+        playerTurn: () => {},
+        initializeLoop: () => {
+            while (game) {
+                if (currentPlayer == 1) {
+                    humanPlayer.row = 1;
+                    humanPlayer.col = 2;
+                } else {
+                }
+            }
+        },
+        get currentPlayer() {
+            return currentPlayer;
+        },
+    };
 };
+
+module.exports = gameLoop;
